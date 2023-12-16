b069b2fbf8ad784b635f79e600056df523c9fb36
Dariagan <seomasini@gmail.com>
2023-12-16 18:31:21 +0100

    gnase

--- zArgentumCpp/SConstruct.py
+++ zArgentumCpp/SConstruct.py
@@ -17,7 +17,11 @@ env = SConscript("godot-cpp/SConstruct")
 # tweak this if you want to use different folders, or more folders, to store your source code in.
 env.Append(CPPPATH=["src/"])
 
-#sudo update-alternatives --config x86_64-w64-mingw32-g++ # Set the default mingw32 g++ compiler option to posix.
+#compilar para windows:
+#1. instalar el compilador:
+#sudo apt-get install gcc-mingw-w64-x86-64 g++-mingw-w64-x86-64 wine64
+#2 poner esto en la Selection=1:
+#sudo update-alternatives --config x86_64-w64-mingw32-g++ # Set the default mingw32 g++ compiler option to posix (1).
 
 env.Append(CXXFLAGS=["-O3", "-fexceptions", "-std=c++17", "-pthread"])
 sources = Glob("src/*.cpp")
